f4953e6d269bbba6cc54ff69759184706401187b
Milan Bojic <bojic.job@gmail.com>
2020-11-16 02:16:00 +0100

    Create race action integration test.

---
+++ DakarRally/DakarRally.IntregrationTests/RallyControllerIntegrationTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DakarRally.IntregrationTests
+{
+    public class RallyControllerIntegrationTests : IClassFixture<TestingWebAppFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+        public RallyControllerIntegrationTests(TestingWebAppFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task CreateRace_SentWrongModel_ReturnsBadRequest()
+        {
+            var postRequest = new HttpRequestMessage(HttpMethod.Post, "/Rally/CreateRace");
+            postRequest.Content = new StringContent("{}", Encoding.UTF8, "application/json");
+            var response = await _client.SendAsync(postRequest);
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+            Assert.Contains("Year is required", responseContent); 
+        }
+
+    }
+}
---
+++ DakarRally/DakarRally.IntregrationTests/TestingWebAppFactory.cs
@@ -0,0 +1,46 @@
+﻿using DakarRally;
+using Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+public class TestingWebAppFactory<T> : WebApplicationFactory<Startup>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+
+            var descriptor = services.SingleOrDefault(
+              d => d.ServiceType ==
+                 typeof(DbContextOptions<RepositoryContext>));
+
+            if (descriptor != null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<RepositoryContext>(o => o.UseSqlite("DataSource=:memory:"));
+
+            var sp = services.BuildServiceProvider();
+
+            using (var scope = sp.CreateScope())
+            {
+                using (var appContext = scope.ServiceProvider.GetRequiredService<RepositoryContext>())
+                {
+                    try
+                    {
+                        appContext.Database.EnsureCreated();
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+                }
+            }
+        });
+    }
+}
